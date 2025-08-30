a9e368ef4341ad3d446e68ac0bbf8cd74a23f5bb
Thảo Phạm Nguyễn Ngọc <pnnthao230194@gmail.com>
2025-08-31 01:55:16 +0700

    Add Trending Service Page

---
+++ src/test/java/projects/userPages/TrendingServicesPage.java
@@ -0,0 +1,16 @@
+package projects.userPages;
+
+import org.openqa.selenium.By;
+
+/*
+ * @Author: Phạm Nguyễn Ngọc Thảo
+ * @Version: 1.0
+ * @Function: Page Trending Service
+ * */
+public class TrendingServicesPage {
+    public static By btnHowFiverrWorks = By.xpath("//button[@class='btn btn-outline-light']");
+    public static By modalHowFiverrWorks = By.xpath("//modal[@class='modalHowFiverrWorks");
+    public static By titleDigitalMarketing = By.xpath("//p[normalize-space()='Digital Marketing']");
+    public static By tagMinimalistLogoDesign = By.xpath("//span[normalize-space()='Minimalist logo design']");
+    public static By itemCategoriesSocialMediaMarketing = By.xpath("//a[@class='categoriesmenu_li_jobdetail_detail_job container'][normalize-space()='Social Media Marketing']");
+}
